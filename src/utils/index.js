bc8178c600456de9b28561b32484cab2ea97dfcd
galangsukamenolong <galangpro280605@gmail.com>
2025-12-11 11:36:55 +0700

    fix(mapping): fix maping db to model

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,20 +1,20 @@
 const mapTaskTabsToModel = (rows) => ({
     id: rows[0].id,
     name: rows[0].name,
-    created_at: rows[0].created_at,
-    delete_permission: rows[0].delete_permission,
+    createdAt: rows[0].created_at,
+    deletePermission: rows[0].delete_permission,
     tasks: rows
         .filter((r) => r.task_id)
         .map((row) => ({
             id: row.task_id,
             title: row.task_title,
             detail: row.task_detail,
-            created_at: row.task_created_at,
+            createdAt: row.task_created_at,
             deadline: row.task_deadline,
-            has_date: row.has_date,
-            has_time: row.has_time,
+            hasDate: row.has_date,
+            hasTime: row.has_time,
             starred: row.starred,
-            is_completed: row.is_completed,
+            isCompleted: row.is_completed,
         })),
 });
 
