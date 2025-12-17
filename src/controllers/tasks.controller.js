92190ad0dd91b58e554d7d3fa7fba8e4284f0273
galangsukamenolong <galangpro280605@gmail.com>
2025-12-17 15:07:02 +0700

    refactor(task): refactor structur flow task

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,9 +1,16 @@
-const TaskService = require('../service/tasks.service');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../exceptions/InvariantError');
+const TaskModel = require('../model/tasks.model');
 
 async function getAllTasksController(req, res, next) {
     const { id } = req.body;
     try {
-        const data = await TaskService.getTaskByIdService(id);
+        const data = await TaskModel.getTaskByIdModel(id);
+
+        if (!data) {
+            throw new InvariantError('Catatan tidak ditemukan');
+        }
+
         res.status(200).json({
             status: 'success',
             data,
@@ -16,7 +23,9 @@ async function getAllTasksController(req, res, next) {
 async function postTaskController(req, res, next) {
     const { title, detail, deadline, hasDate, hasTime, starred, isCompleted, taskTabId } = req.body;
     try {
-        const result = await TaskService.addTaskService({
+        const id = `task-${nanoid(16)}`;
+
+        const result = await TaskModel.addTaskModel(id, {
             title,
             detail,
             deadline,
@@ -26,6 +35,11 @@ async function postTaskController(req, res, next) {
             isCompleted,
             taskTabId,
         });
+
+        if (!result) {
+            throw new InvariantError('Gagal menambahkan catatan');
+        }
+
         res.status(201).json({
             status: 'success',
             message: 'Task berhasil ditambahkan',
@@ -53,8 +67,17 @@ async function patchTaskController(req, res, next) {
         values.push(isCompleted ? 1 : 0);
     }
 
+    if (field.length === 0) {
+        throw new InvariantError('Tidak ada field yang diperbarui');
+    }
+
     try {
-        const result = await TaskService.patchTaskService(id, field, values);
+        const result = await TaskModel.updateTaskModel(id, field, values);
+
+        if (!result) {
+            throw new InvariantError('Gagal memperbarui catatan');
+        }
+
         res.status(200).json({
             status: 'success',
             message: 'Catatan berhasil diperbaharui',
