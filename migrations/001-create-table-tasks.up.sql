a0fa791b3bed79e066969c8f15f17fcf75e89293
galangsukamenolong <galangpro280605@gmail.com>
2025-12-17 15:06:38 +0700

    fix(type): change varchar lenght

--- migrations/001-create-table-tasks.up.sql
+++ migrations/001-create-table-tasks.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS tasks (
-    id VARCHAR(30) NOT NULL PRIMARY KEY,
+    id VARCHAR(50) NOT NULL PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     detail TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS tasks (
     has_time BOOLEAN NOT NULL DEFAULT false,
     starred BOOLEAN NOT NULL DEFAULT false,
     is_completed BOOLEAN NOT NULL DEFAULT false,
-    task_tabs_id VARCHAR(30) NOT NULL
+    task_tabs_id VARCHAR(50) NOT NULL
 );
