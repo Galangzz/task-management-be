627a66a5aae73bb3b3685466dcabedd2488c8fc2
galangsukamenolong <galangpro280605@gmail.com>
2026-01-09 18:07:22 +0700

    feat(login): add limiter

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { rateLimit } = require('express-rate-limit');
 
 const methodNotAllowed = require('../middlewares/methdoNotAllowedHandler');
 const { validate } = require('../middlewares/validate');
@@ -7,17 +8,26 @@ const asyncHandler = require('../middlewares/asyncHandler');
 const { postAuthSchema, putAuthSchema, deleteAuthSchema } = require('../validator/authSchema');
 const AuthController = require('../controllers/authController');
 
+const loginLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    limit: 5,
+    message: 'Terlalu banyak percobaan login, silakan coba lagi nanti.',
+    standardHeaders: true,
+    legacyHeaders: false,
+    // skip: (req) => req.method === 'OPTIONS',
+});
+
 const router = express.Router();
 
 // /api/auth
 router
     .route('/login')
-    .post(validate(postAuthSchema, 'body'), asyncHandler(AuthController.postAuthController))
+    .post(loginLimiter, validate(postAuthSchema, 'body'), asyncHandler(AuthController.postAuthController))
     .all(methodNotAllowed(['POST']));
 
 router
     .route('/refresh')
-    .get(validate(putAuthSchema, 'cookies'), asyncHandler(AuthController.getNewAccessToken))
+    .get(validate(putAuthSchema, 'cookies'), AuthController.getNewAccessToken)
     .all(methodNotAllowed(['GET']));
 
 router
